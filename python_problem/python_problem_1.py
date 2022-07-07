bc36a92a149d99ab7e0ccc1412836e8710091079
witwint <witwint1234@gmail.com>
2022-07-08 01:46:38 +0900

    파이썬1-4

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -10,3 +10,6 @@ while (count != '1' and count != '2' and count != '3'):
         print("1,2,3 중 하나를 입력하세요")
         count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
 
+for i in range(0,int(count)):
+    num += 1
+    print('playerA : ',num)
