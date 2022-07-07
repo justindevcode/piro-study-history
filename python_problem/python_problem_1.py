e80fe4fe59dc54e73a4a9068387c37040ae28520
witwint <witwint1234@gmail.com>
2022-07-08 01:57:29 +0900

    파이썬 1-6

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,31 +1,44 @@
 
 num=0
-count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+lose= None
+while True:
+    count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
 
-while (count != '1' and count != '2' and count != '3'):
-    if not count.isdigit() :
-        print('정수를 입력하세요')
-        count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
-    else :
-        print("1,2,3 중 하나를 입력하세요")
-        count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+    while (count != '1' and count != '2' and count != '3'):
+        if not count.isdigit() :
+            print('정수를 입력하세요')
+            count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+        else :
+            print("1,2,3 중 하나를 입력하세요")
+            count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
 
-for i in range(0,int(count)):
-    num += 1
-    print('playerA : ',num)
+    for i in range(0,int(count)):
+        num += 1
+        print('playerA : ', num)
+        if num == 31:
+            lose = "A";
+            break;
+
+    if lose != None:
+        break
 
 
+    count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
 
-count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+    while (count != '1' and count != '2' and count != '3'):
+        if not count.isdigit() :
+            print('정수를 입력하세요')
+            count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+        else :
+            print("1,2,3 중 하나를 입력하세요")
+            count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
 
-while (count != '1' and count != '2' and count != '3'):
-    if not count.isdigit() :
-        print('정수를 입력하세요')
-        count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
-    else :
-        print("1,2,3 중 하나를 입력하세요")
-        count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
-
-for i in range(0,int(count)):
-    num += 1
-    print('playerB : ',num)
+    for i in range(0,int(count)):
+        num += 1
+        print('playerB : ', num)
+        if num == 31:
+            lose = "A";
+            break;
+    if lose != None:
+        break
+print('게임끝')
