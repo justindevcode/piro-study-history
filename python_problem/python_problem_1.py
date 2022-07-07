5024c38b935613b0e127a5e5b45575fa4a9ea436
witwint <witwint1234@gmail.com>
2022-07-08 01:43:49 +0900

    파이썬1-3

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,2 +1,12 @@
+
 num=0
-count = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+
+while (count != '1' and count != '2' and count != '3'):
+    if not count.isdigit() :
+        print('정수를 입력하세요')
+        count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+    else :
+        print("1,2,3 중 하나를 입력하세요")
+        count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+
