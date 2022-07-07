715d98add2782640698ab832e37babd6e436a124
witwint <witwint1234@gmail.com>
2022-07-08 01:48:36 +0900

    파이썬1-5

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -13,3 +13,19 @@ while (count != '1' and count != '2' and count != '3'):
 for i in range(0,int(count)):
     num += 1
     print('playerA : ',num)
+
+
+
+count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+
+while (count != '1' and count != '2' and count != '3'):
+    if not count.isdigit() :
+        print('정수를 입력하세요')
+        count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+    else :
+        print("1,2,3 중 하나를 입력하세요")
+        count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+
+for i in range(0,int(count)):
+    num += 1
+    print('playerB : ',num)
