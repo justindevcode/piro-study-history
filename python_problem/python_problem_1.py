87f16a7bd7e14f6c62c2e2f6f69cbd4d94cdbb47
witwint <witwint1234@gmail.com>
2022-07-08 02:35:00 +0900

    파이썬1-9

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,4 +1,4 @@
-
+import random
 
 num = 0
 global lose
@@ -6,38 +6,41 @@ lose = None
 
 def brGame(A , num):
     num2 = num
-    count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+    if A == "computer":
+        count = random.randint(1, 3)
 
-    while (count != '1' and count != '2' and count != '3'):
-        if not count.isdigit():
-            print('정수를 입력하세요')
-            count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
-        else:
-            print("1,2,3 중 하나를 입력하세요")
-            count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+    else:
+        count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+
+        while (count != '1' and count != '2' and count != '3'):
+            if not count.isdigit():
+                print('정수를 입력하세요')
+                count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+            else:
+                print("1,2,3 중 하나를 입력하세요")
+                count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
 
     for i in range(0, int(count)):
         num2 += 1
-        print('player',A,' : ', num2)
+        print(A,' : ', num2)
         if num2 == 31:
             break
     if num2 == 31:
-        return "B"
+        return A
     else:
         return int(count)
 
 
 while True:
-    resolt = brGame('A', num)
-    if resolt == 'B':
-        print('playerB win!')
+    resolt = brGame('computer', num)
+    if resolt == 'computer':
+        print('player win!')
         break
     else:
         num +=resolt
-    resolt = brGame('B', num)
-    if resolt == 'A':
-        print('playerA win!')
-        num += resolt
+    resolt = brGame('player', num)
+    if resolt == 'player':
+        print('computer win!')
         break
     else:
         num += resolt
