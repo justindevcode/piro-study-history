e7dbda3e893584c508d233d2d0afcc6112f0262e
witwint <witwint1234@gmail.com>
2022-07-08 02:24:35 +0900

    파이썬1-8

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,46 +1,44 @@
 
-num=0
-lose= None
+
+num = 0
+global lose
+lose = None
+
+def brGame(A , num):
+    num2 = num
+    count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+
+    while (count != '1' and count != '2' and count != '3'):
+        if not count.isdigit():
+            print('정수를 입력하세요')
+            count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+        else:
+            print("1,2,3 중 하나를 입력하세요")
+            count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+
+    for i in range(0, int(count)):
+        num2 += 1
+        print('player',A,' : ', num2)
+        if num2 == 31:
+            break
+    if num2 == 31:
+        return "B"
+    else:
+        return int(count)
+
+
 while True:
-    count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
-
-    while (count != '1' and count != '2' and count != '3'):
-        if not count.isdigit() :
-            print('정수를 입력하세요')
-            count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
-        else :
-            print("1,2,3 중 하나를 입력하세요")
-            count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
-
-    for i in range(0,int(count)):
-        num += 1
-        print('playerA : ', num)
-        if num == 31:
-            lose = "A";
-            win = "B"
-            break;
-
-    if lose != None:
+    resolt = brGame('A', num)
+    if resolt == 'B':
+        print('playerB win!')
         break
-
-
-    count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
-
-    while (count != '1' and count != '2' and count != '3'):
-        if not count.isdigit() :
-            print('정수를 입력하세요')
-            count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
-        else :
-            print("1,2,3 중 하나를 입력하세요")
-            count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
-
-    for i in range(0,int(count)):
-        num += 1
-        print('playerB : ', num)
-        if num == 31:
-            lose = "B";
-            win = "A"
-            break;
-    if lose != None:
+    else:
+        num +=resolt
+    resolt = brGame('B', num)
+    if resolt == 'A':
+        print('playerA win!')
+        num += resolt
         break
-print("player",win,"win!")
+    else:
+        num += resolt
+
